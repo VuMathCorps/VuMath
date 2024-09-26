6ecae31938adbc1cd5106da676b1ecf5c4c641d2
Amanuel Tamrat <amanuel0625@gmail.com>
2024-09-25 23:32:49 -0500

    Moved side bar functions to another file to reduce repetition

---
+++ load-sidebar.js
@@ -0,0 +1,20 @@
+function loadSidebar() {
+    fetch('sidebar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('afterbegin', data);
+        })
+        .catch(error => console.error('Error loading sidebar:', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadSidebar);
+
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
