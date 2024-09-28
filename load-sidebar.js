7583f85e2716c37835aa513a1cf307b2ae6127fc
Amanuel Tamrat <amanuel0625@gmail.com>
2024-09-27 22:37:19 -0500

    Changed side bar width

--- load-sidebar.js
+++ load-sidebar.js
@@ -10,8 +10,8 @@ function loadSidebar() {
 document.addEventListener('DOMContentLoaded', loadSidebar);
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "280px";
+    document.getElementById("main").style.marginLeft = "280px";
 }
 
 function closeNav() {
